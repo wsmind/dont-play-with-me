5f5e6e60f71d4c8f57d386678e2bf37aa30f13fc
Mangatome <cybisoft@free.fr>
2013-01-26 11:43:48 +0100

    hero jump, gravity collision

--- game/Config.lua
+++ game/Config.lua
@@ -5,7 +5,12 @@
 require("math.vec2")
 
 Config = {
-	heroHorizontalSpeed = vec2(5,0), -- How much the hero moves when it's walking to the right
-	heroVerticalSpeed = vec2(0,-5), -- How much the hero moves when it's jumping upwards
-	gravity = vec2(0,9) -- Strength of the gravity
+	-- Game space configuration 
+	virtualScreenHeight = 1080,
+	
+	-- Player avatar configuration
+	heroHorizontalSpeed = vec2(500,0), -- How much the hero moves when it's walking to the right (px/s)
+	heroVerticalSpeed = vec2(0,-500), -- How much the hero moves when it's jumping upwards (px/s)
+	heroJumpDuration = 1, -- How long does the ascending part of the jump last, in seconds
+	gravity = vec2(0,1000) -- Strenght of the gravity
 }
