a8a4ebe919b9fa967b4bc0887c128662bbfc4d3b
Remi Papillie <remi.papillie@gmail.com>
2013-01-26 21:36:19 +0100

    fixed background heartbeat

--- game/Background.lua
+++ game/Background.lua
@@ -24,7 +24,6 @@ function Background.new(options)
 	self.nextBreakTime = 5
 	
 	self.image = love.graphics.newImage("assets/background/Gradient.png")
-	self.image:setFilter("nearest","nearest")
 	self.imageScaleY = love.graphics.getHeight() / self.image:getHeight()
 	self.imageScaleX = love.graphics.getWidth() / self.image:getWidth()
 	
@@ -32,19 +31,19 @@ function Background.new(options)
 end
 
 function Background:update(dt)
-	self.currentTime = self.currentTime + dt
+	self.currentTime = self.currentTime + dt * (1 + self.mood.excitement)
 	
 	-- updates the pulse parameters according to mood.
-	self.pulseBreakDuration = 5 * (1 - self.mood.excitement) + 0.5
+	self.pulseBreakDuration = 2 --* (1 - self.mood.excitement) + 0.5
 	
 	-- pulses the background color
 	local baseColor = self.mood:getColorVec4()
 	local shift = math.sin(self.currentTime * Config.bgPulseColorSpeed) * Config.bgPulseColorAmplitude -- gets the color shift
 	baseColor = (baseColor + vec4(shift, shift, shift, shift)):clamp(0, 255) -- shiffted color
-	local finalFactor = math.exp(-math.fmod(self.currentTime, self.pulseBreakDuration)) * self.pulseDuration -- break and shift factor
-	baseColor = baseColor * finalFactor -- shiffted color + breaks
-	self.color = baseColor
-	--self.color = self.mood:getColorVec4()
+	local finalFactorFirstBeat = math.exp(-math.fmod(self.currentTime, self.pulseDuration) * 0.9) -- break and shift factor
+	local finalFactorSecondBeat = math.exp(-math.fmod(self.currentTime - 0.5, self.pulseDuration) * 0.8) * 0.8 -- break and shift factor
+	baseColor = baseColor * (finalFactorFirstBeat + finalFactorSecondBeat) -- shiffted color + breaks
+	self.color = baseColor:clamp(0, 255)
 end
 
 function Background:draw()
